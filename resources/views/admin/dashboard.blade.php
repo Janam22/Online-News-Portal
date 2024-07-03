076dda5a9a5a7bce50f506a40d992d71e10046b2
Janam Pandey <jananpandey1995@gmail.com>
2024-07-03 23:39:34 +0545

    added one line

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('categoryadmin.layouts.master')
 @section('content')
 
-<p>Hello Admin</p>
+<p>Hello Admin - I am add_feature.</p>
 <input type="text" class="form-control" name="name">
 
 @endsection
