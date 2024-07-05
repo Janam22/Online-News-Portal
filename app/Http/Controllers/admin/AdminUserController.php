a097078b1782f40c16e99697a8412a8d33e09cca
Janam Pandey <jananpandey1995@gmail.com>
2024-07-05 23:49:23 +0545

    added custom query to fetch users data

--- app/Http/Controllers/admin/AdminUserController.php
+++ app/Http/Controllers/admin/AdminUserController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers\admin;
 use App\Models\User;
-
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
     public function manageusers(){
         
-        $manageusers = User::all();
+        $manageusers = DB::select('SELECT * FROM users WHERE type !=?', [2]);
         return view('admin.manage_users.users', compact('manageusers'));
     }
 }
